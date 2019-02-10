2c10a90dfbc4718c81ed530f59ef83747f7f87ac
Robert Norman <rnorman@tripadvisor.com>
2019-02-10 18:29:33 +0000

    just playing around

---
+++ pages/test/index.vue
@@ -0,0 +1,56 @@
+<template>
+    <section class="container">
+    <div>
+      <h1 class="title">
+        first-project Test
+      </h1>
+      <h2 class="subtitle">
+        Test component as a page hype!
+      </h2>
+        <a
+          href="https://www.holidaywatchdog.com"
+          target="_blank"
+          class="button--grey"
+        >HWD</a>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
